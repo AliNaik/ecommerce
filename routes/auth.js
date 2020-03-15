3dae985e2a50e9789037374792f500616a458785
Ali <ali@hexwhale.com>
2020-03-15 07:52:49 +0530

    push to github

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin
+} = require("../controllers/auth");
+const { userSignupValidator } = require("../validator");
+
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+
+//to check requireSignin middleware is working or not
+
+// router.get("/hello", requireSignin, (req, res) => {
+//   res.send("hello there");
+// });
+
+module.exports = router;
